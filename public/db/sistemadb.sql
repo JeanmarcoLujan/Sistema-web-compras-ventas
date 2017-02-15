79a9e832258914650cba293575e24312d3353923
Jeanmarco Lujan <mark.15@hotmail.es>
2017-02-14 19:58:37 -0500

    agregando la base de datos sistemadb

--- public/db/sistemadb.sql
+++ public/db/sistemadb.sql
@@ -1,147 +1,207 @@
 
-use sistemadb;
-
-CREATE  TABLE IF NOT EXISTS `categorias` (
-  `idcategoria` INT NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(50) NOT NULL ,
-  `descripcion` VARCHAR(256) NULL ,
-  `condicion` TINYINT(1) NOT NULL ,
-  PRIMARY KEY (`idcategoria`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`articulo`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `articulos` (
-  `idarticulo` INT NOT NULL AUTO_INCREMENT ,
-  `idcategoria` INT NOT NULL ,
-  `codigo` VARCHAR(50) NULL ,
-  `nombre` VARCHAR(100) NOT NULL ,
-  `stock` INT NOT NULL ,
-  `descripcion` VARCHAR(512) NULL ,
-  `imagen` VARCHAR(50) NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idarticulo`) ,
-  INDEX `fk_articulo_categoria_idx` (`idcategoria` ASC) ,
-  CONSTRAINT `fk_articulo_categoria`
-    FOREIGN KEY (`idcategoria` )
-    REFERENCES `categorias` (`idcategoria` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `articulos` (
+  `idarticulo` int(11) NOT NULL AUTO_INCREMENT,
+  `idcategoria` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `codigo` varchar(50) DEFAULT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `imagen` varchar(50) DEFAULT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`idarticulo`),
+  KEY `fk_articulo_categoria_idx` (`idcategoria`),
+  KEY `unit_id` (`unit_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`persona`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `personas` (
-  `idpersona` INT NOT NULL AUTO_INCREMENT ,
-  `tipo_persona` VARCHAR(20) NOT NULL ,
-  `nombre` VARCHAR(100) NOT NULL ,
-  `tipo_documento` VARCHAR(20) NULL ,
-  `num_documento` VARCHAR(15) NULL ,
-  `direccion` VARCHAR(70) NULL ,
-  `telefono` VARCHAR(15) NULL ,
-  `email` VARCHAR(50) NULL ,
-  PRIMARY KEY (`idpersona`) )
-ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`ingreso`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ingresos` (
-  `idingreso` INT NOT NULL AUTO_INCREMENT ,
-  `idproveedor` INT NOT NULL ,
-  `tipo_comprobante` VARCHAR(20) NOT NULL ,
-  `serie_comprobante` VARCHAR(7) NULL ,
-  `num_comprobante` VARCHAR(10) NOT NULL ,
-  `fecha_hora` DATETIME NOT NULL ,
-  `impuesto` DECIMAL(4,2) NOT NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idingreso`) ,
-  INDEX `fk_ingreso_persona_idx` (`idproveedor` ASC) ,
-  CONSTRAINT `fk_ingreso_persona`
-    FOREIGN KEY (`idproveedor` )
-    REFERENCES `personas` (`idpersona` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `detalle_ingresos` (
+  `iddetalle_ingreso` int(11) NOT NULL AUTO_INCREMENT,
+  `idingreso` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta_unitario` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`iddetalle_ingreso`),
+  KEY `fk_detalle_ingreso_idx` (`idingreso`),
+  KEY `fk_detalle_ingreso_articulo_idx` (`idarticulo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`detalle_ingreso`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `detalle_ingresos` (
-  `iddetalle_ingreso` INT NOT NULL AUTO_INCREMENT ,
-  `idingreso` INT NOT NULL ,
-  `idarticulo` INT NOT NULL ,
-  `cantidad` INT NOT NULL ,
-  `precio_compra` DECIMAL(11,2) NOT NULL ,
-  `precio_venta` DECIMAL(11,2) NOT NULL ,
-  PRIMARY KEY (`iddetalle_ingreso`) ,
-  INDEX `fk_detalle_ingreso_idx` (`idingreso` ASC) ,
-  INDEX `fk_detalle_ingreso_articulo_idx` (`idarticulo` ASC) ,
-  CONSTRAINT `fk_detalle_ingreso`
-    FOREIGN KEY (`idingreso` )
-    REFERENCES `ingresos` (`idingreso` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_detalle_ingreso_articulo`
-    FOREIGN KEY (`idarticulo` )
-    REFERENCES `articulos` (`idarticulo` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
+DROP TRIGGER IF EXISTS `tr_upStockIngreso`;
+DELIMITER //
+CREATE TRIGGER `tr_upStockIngreso` AFTER INSERT ON `detalle_ingresos`
+ FOR EACH ROW UPDATE articulos SET stock = stock + NEW.cantidad WHERE articulos.idarticulo = NEW.idarticulo
+//
+DELIMITER ;
 
 
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ventas` (
-  `idventa` INT NOT NULL AUTO_INCREMENT ,
-  `idcliente` INT NOT NULL ,
-  `tipo_comprobante` VARCHAR(20) NOT NULL ,
-  `serie_comprobante` VARCHAR(7) NOT NULL ,
-  `num_comprobante` VARCHAR(10) NOT NULL ,
-  `fecha_hora` DATETIME NOT NULL ,
-  `impuesto` DECIMAL(4,2) NOT NULL ,
-  `total_venta` DECIMAL(11,2) NOT NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idventa`) ,
-  INDEX `fk_venta_cliente_idx` (`idcliente` ASC) ,
-  CONSTRAINT `fk_venta_cliente`
-    FOREIGN KEY (`idcliente` )
-    REFERENCES `personas` (`idpersona` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `detalle_ventas` (
+  `iddetalle_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `idventa` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) DEFAULT NULL,
+  PRIMARY KEY (`iddetalle_venta`),
+  KEY `fk_detalle_venta_articulo_idx` (`idarticulo`),
+  KEY `fk_detalle_venta_idx` (`idventa`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 
--- -----------------------------------------------------
--- Table `dbventaslaravel`.`detalle_venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `detalle_ventas` (
-  `iddetalle_venta` INT NOT NULL AUTO_INCREMENT ,
-  `idventa` INT NOT NULL ,
-  `idarticulo` INT NOT NULL ,
-  `cantidad` INT NOT NULL ,
-  `precio_venta` DECIMAL(11,2) NOT NULL ,
-  `descuento` DECIMAL(11,2) NOT NULL ,
-  PRIMARY KEY (`iddetalle_venta`) ,
-  INDEX `fk_detalle_venta_articulo_idx` (`idarticulo` ASC) ,
-  INDEX `fk_detalle_venta_idx` (`idventa` ASC) ,
-  CONSTRAINT `fk_detalle_venta_articulo`
-    FOREIGN KEY (`idarticulo` )
-    REFERENCES `articulos` (`idarticulo` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_detalle_venta`
-    FOREIGN KEY (`idventa` )
-    REFERENCES `ventas` (`idventa` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TRIGGER IF EXISTS `tr_upStockVenta`;
+DELIMITER //
+CREATE TRIGGER `tr_upStockVenta` AFTER INSERT ON `detalle_ventas`
+ FOR EACH ROW UPDATE articulos SET stock = stock - NEW.cantidad WHERE articulos.idarticulo = NEW.idarticulo
+//
+DELIMITER ;
 
+
+
+CREATE TABLE IF NOT EXISTS `ingresos` (
+  `idingreso` int(11) NOT NULL AUTO_INCREMENT,
+  `idproveedor` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) DEFAULT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`idingreso`),
+  KEY `fk_ingreso_persona_idx` (`idproveedor`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL,
+  KEY `password_resets_email_index` (`email`),
+  KEY `password_resets_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE IF NOT EXISTS `personas` (
+  `idpersona` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_persona` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo_documento` varchar(20) DEFAULT NULL,
+  `num_documento` varchar(15) DEFAULT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idpersona`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+
+CREATE TABLE IF NOT EXISTS `units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(55) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `foto` varchar(33) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `foto`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Alberto Jeanmarco', 'Luján Ventura', 'mark@hotmail.es', '$2y$10$eyapGy07Q1396xKgx4Z/4uQt6FhT7UjFv7bz1/bXbc93iiS2xIFCe', 'Koala.jpg', NULL, '2016-12-12 01:18:07', '2016-12-12 01:18:07');
+
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `idventa` int(11) NOT NULL AUTO_INCREMENT,
+  `idcliente` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) NOT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`idventa`),
+  KEY `fk_venta_cliente_idx` (`idcliente`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
+ALTER TABLE `articulos`
+  ADD CONSTRAINT `fk_articulo_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_articulo_unit` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`);
+
+--
+-- Filtros para la tabla `detalle_ingresos`
+--
+ALTER TABLE `detalle_ingresos`
+  ADD CONSTRAINT `fk_detalle_ingreso` FOREIGN KEY (`idingreso`) REFERENCES `ingresos` (`idingreso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ingreso_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulos` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `fk_detalle_venta` FOREIGN KEY (`idventa`) REFERENCES `ventas` (`idventa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_venta_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulos` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD CONSTRAINT `fk_ingreso_persona` FOREIGN KEY (`idproveedor`) REFERENCES `personas` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`idcliente`) REFERENCES `personas` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
